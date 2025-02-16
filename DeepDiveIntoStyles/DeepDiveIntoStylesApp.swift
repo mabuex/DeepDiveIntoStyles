de532e13ca8a36169623d6efb70087a02e94e3ad
Marcus Buexenstein <mabuex@gmail.com>
2025-02-16 09:07:38 +0900

    Initial Commit

---
+++ DeepDiveIntoStyles/DeepDiveIntoStylesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeepDiveIntoStylesApp.swift
+//  DeepDiveIntoStyles
+//
+//  Created by Marcus Buexenstein on 2/16/25.
+//
+
+import SwiftUI
+
+@main
+struct DeepDiveIntoStylesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
